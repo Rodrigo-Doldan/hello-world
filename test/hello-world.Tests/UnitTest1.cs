8a62ba0b2d4be717c6c7a9232d4f588c9adde544
Rodrigo-Doldan <rodrigodoldan04@gmail.com>
2023-08-21 20:21:47 -0300

    Agregar proyecto C#

---
+++ test/hello-world.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace hello_world.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
